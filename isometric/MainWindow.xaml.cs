d6db39e58bf2e620d7be4a9cb05a48cbeb074dd0
Nathan Rozendaal <nathan.rozendaal@gmail.com>
2019-10-22 11:52:27 +0200

    minor refactor

--- isometric/MainWindow.xaml.cs
+++ isometric/MainWindow.xaml.cs
@@ -38,22 +38,17 @@ namespace isometric
 
 		public MainWindow()
 		{
-
 			spritesB = new BitmapImage(new Uri("pack://application:,,,/isometric_demo2.png"));
 			pWorld = new int[Convert.ToInt32(vWorldsize.X * vWorldsize.Y)];
 			Array.Clear(pWorld, 0, pWorld.Length);
 			bitmap = BitmapImage2Bitmap(spritesB);
 			InitializeComponent();
-
 			StartUI();
 			CompositionTarget.Rendering += CompositionTarget_Rendering;
-
-
 		}
 
 		private void StartUI()
 		{
-
 			canvas.Children.Clear();
 			int counter = 0;
 			for (int y = 0; y < vWorldsize.Y; y++)
@@ -61,36 +56,7 @@ namespace isometric
 				for (int x = 0; x < vWorldsize.X; x++)
 				{
 					Vector vWorld = ToScreen(x, y);
-					Image image = new Image();
-					switch (pWorld[y * Convert.ToInt32(vWorldsize.X) + x])
-					{
-						
-						case 0:
-
-							image = (DrawPartialSprite(Convert.ToInt32(vWorld.X), Convert.ToInt32(vWorld.Y), 1 * Convert.ToInt32(vTileSize.X), 0, Convert.ToInt32(vTileSize.X), Convert.ToInt32(vTileSize.Y)));
-
-							break;
-						case 1:
-							// Visible Tile
-							image = (DrawPartialSprite(Convert.ToInt32(vWorld.X), Convert.ToInt32(vWorld.Y), 2 * Convert.ToInt32(vTileSize.X), 0, Convert.ToInt32(vTileSize.X), Convert.ToInt32(vTileSize.Y)));
-							break;
-						case 2:
-							// Tree
-							image = (DrawPartialSprite(Convert.ToInt32(vWorld.X), Convert.ToInt32(vWorld.Y) - Convert.ToInt32(vTileSize.Y), 0 * Convert.ToInt32(vTileSize.X), 1 * Convert.ToInt32(vTileSize.Y), Convert.ToInt32(vTileSize.X), Convert.ToInt32(vTileSize.Y*2)));
-							break;
-						case 3:
-							// Spooky Tree
-							image = (DrawPartialSprite(Convert.ToInt32(vWorld.X), Convert.ToInt32(vWorld.Y) - Convert.ToInt32(vTileSize.Y), 1 * Convert.ToInt32(vTileSize.X), 1 * Convert.ToInt32(vTileSize.Y), Convert.ToInt32(vTileSize.X), Convert.ToInt32(vTileSize.Y * 2)));
-							break;
-						case 4:
-							// Beach
-							image = (DrawPartialSprite(Convert.ToInt32(vWorld.X), Convert.ToInt32(vWorld.Y) - Convert.ToInt32(vTileSize.Y), 2 * Convert.ToInt32(vTileSize.X), 1 * Convert.ToInt32(vTileSize.Y), Convert.ToInt32(vTileSize.X), Convert.ToInt32(vTileSize.Y * 2)));
-							break;
-						case 5:
-							// Water
-							image = (DrawPartialSprite(Convert.ToInt32(vWorld.X), Convert.ToInt32(vWorld.Y) - Convert.ToInt32(vTileSize.Y), 3 * Convert.ToInt32(vTileSize.X), 1 * Convert.ToInt32(vTileSize.Y), Convert.ToInt32(vTileSize.X), Convert.ToInt32(vTileSize.Y * 2)));
-							break;
-					}
+					Image image = GetTile(vWorld, y * Convert.ToInt32(vWorldsize.X) + x);
 					canvas.Children.Add(image);
 					images[counter] = image;
 					counter++;
@@ -103,14 +69,24 @@ namespace isometric
 			int x = Convert.ToInt32(v.X);
 			int y = Convert.ToInt32(v.Y);
 			Vector vWorld = ToScreen(x, y);
-			Image image = new Image();
-			switch (pWorld[id])
+			Image image = GetTile(vWorld,id);
+			images[id] = image;
+			canvas.Children.CopyTo(uIElements,0);
+			uIElements[id] = image;
+			canvas.Children.Clear();
+			foreach (var item in uIElements)
 			{
-
+				canvas.Children.Add(item);
+			}
+		}
+		private Image GetTile(Vector vWorld, int index)
+		{
+			Image image = new Image();
+			switch (pWorld[index])
+			{
 				case 0:
-
+					// Empty Tile
 					image = (DrawPartialSprite(Convert.ToInt32(vWorld.X), Convert.ToInt32(vWorld.Y), 1 * Convert.ToInt32(vTileSize.X), 0, Convert.ToInt32(vTileSize.X), Convert.ToInt32(vTileSize.Y)));
-
 					break;
 				case 1:
 					// Visible Tile
@@ -132,31 +108,16 @@ namespace isometric
 					// Water
 					image = (DrawPartialSprite(Convert.ToInt32(vWorld.X), Convert.ToInt32(vWorld.Y) - Convert.ToInt32(vTileSize.Y), 3 * Convert.ToInt32(vTileSize.X), 1 * Convert.ToInt32(vTileSize.Y), Convert.ToInt32(vTileSize.X), Convert.ToInt32(vTileSize.Y * 2)));
 					break;
-
 			}
-			images[id] = image;
-			canvas.Children.CopyTo(uIElements,0);
-			for (int i = 0; i < uIElements.Length; i++)
-			{
-				if (i == id)
-				{
-					uIElements[id] = image;
-				}
-			}
-			canvas.Children.Clear();
-			foreach (var item in uIElements)
-			{
-				canvas.Children.Add(item);
-			}
-			
+			return image;
 		}
 
-		private System.Windows.Controls.Image DrawPartialSprite(int locx, int locy, int px, int py, int width, int height)
+		private Image DrawPartialSprite(int locx, int locy, int px, int py, int width, int height)
 		{
 			CroppedBitmap cb = new CroppedBitmap(
 							spritesB,
 							new Int32Rect(1 * px, py, width, height));
-			System.Windows.Controls.Image cbi = new System.Windows.Controls.Image
+			Image cbi = new Image
 			{
 				Width = cb.PixelWidth,
 				Height = cb.PixelHeight,
@@ -190,25 +151,19 @@ namespace isometric
 			double b = (vOrigin.Y * vTileSize.Y) + (x + y) * (vTileSize.Y / 2);
 			return new Vector(a, b);
 		}
-		System.Windows.Controls.Image remove;
+		Image remove;
 		//game loop
 		private void CompositionTarget_Rendering(object sender, EventArgs e)
 		{
-
 			Vector vMouse = new Vector(GetMousePos().X, GetMousePos().Y);
 			Vector vCell = new Vector(Math.Floor(vMouse.X / vTileSize.X), Math.Floor(vMouse.Y / vTileSize.Y));
-
 			Vector vOffset = new Vector(vMouse.X % vTileSize.X, vMouse.Y % vTileSize.Y);
 
-
-			vSelected = new Vector();
 			vSelected.X = (vCell.Y - vOrigin.Y) + (vCell.X - vOrigin.X);
 			vSelected.Y = (vCell.Y - vOrigin.Y) - (vCell.X - vOrigin.X);
-			Color col = Color.White;
 			if (vOffset.X > 0 && vOffset.Y > 0)
 			{
-
-				col = bitmap.GetPixel(Convert.ToInt32(3 * vTileSize.X + vOffset.X), Convert.ToInt32(vOffset.Y));
+				Color col = bitmap.GetPixel(Convert.ToInt32(3 * vTileSize.X + vOffset.X), Convert.ToInt32(vOffset.Y));
 				if (col == Color.FromArgb(255, 0, 0))
 				{
 					vSelected.X += -1;
@@ -229,22 +184,14 @@ namespace isometric
 					vSelected.X += +1;
 					vSelected.Y += 0;
 				}
-
 			}
-
-
-
-
 
 			Vector vSelectedWorld = ToScreen(Convert.ToInt32(vSelected.X), Convert.ToInt32(vSelected.Y));
 			canvas.Children.Remove(remove);
-			System.Windows.Controls.Image img = DrawPartialSprite(Convert.ToInt32(vSelectedWorld.X), Convert.ToInt32(vSelectedWorld.Y), 0, 0, Convert.ToInt32(vTileSize.X), Convert.ToInt32(vTileSize.Y));
+			Image img = DrawPartialSprite(Convert.ToInt32(vSelectedWorld.X), Convert.ToInt32(vSelectedWorld.Y), 0, 0, Convert.ToInt32(vTileSize.X), Convert.ToInt32(vTileSize.Y));
 			canvas.Children.Add(img);
-
 			remove = img;
-
-			info.Text = $"Mouse \t: {vMouse.X} {vMouse.Y}\nCell \t: {vCell.X} {vCell.Y}\nSelected\t:{vSelected.X} {vSelected.Y}";
-
+			info.Text = $"Mouse \t: {vMouse.X},{vMouse.Y}\nCell \t: {vCell.X},{vCell.Y}\nSelected\t: {vSelected.X},{vSelected.Y}";
 		}
 
 		private void clickevent(object sender, MouseButtonEventArgs e)
@@ -261,11 +208,6 @@ namespace isometric
 				}
 				UpdateUI(Convert.ToInt32(vSelected.Y * vWorldsize.X + vSelected.X), vSelected);
 			}
-
-
-			
-
-
 		}
 	}
 }
